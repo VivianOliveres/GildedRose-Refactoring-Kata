2d2e09ec3673c288f23989059648a7a83fb27201
v.oliveres <v.oliveres@criteo.com>
2017-12-20 11:39:42 +0100

    Add tests for AgedBrie

--- src/test/scala/com/gildedrose/GildedRoseTest.scala
+++ src/test/scala/com/gildedrose/GildedRoseTest.scala
@@ -2,11 +2,38 @@ package com.gildedrose
 
 import org.scalatest._
 
-class GildedRoseTest  extends FlatSpec with Matchers {
-  it should "foo" in {
-    var items = Array[Item](new Item("foo", 0, 0))
+class GildedRoseTest extends FlatSpec with Matchers {
+
+  it should "[Aged Brie] increments quality and decrements sellIn" in {
+    val itemAgedBrie = new Item("Aged Brie", 123, 15)
+    val items = Array[Item](itemAgedBrie)
     val app = new GildedRose(items)
+
     app.updateQuality()
-    (app.items(0).name) should equal ("fixme")
+
+    itemAgedBrie.sellIn shouldBe 122
+    itemAgedBrie.quality shouldBe 16
+  }
+
+  it should "[Aged Brie] increments quality by 2 and decrements sellIn when sellIn becomes negative" in {
+    val itemAgedBrie = new Item("Aged Brie", 0, 15)
+    val items = Array[Item](itemAgedBrie)
+    val app = new GildedRose(items)
+
+    app.updateQuality()
+
+    itemAgedBrie.sellIn shouldBe -1
+    itemAgedBrie.quality shouldBe 17
+  }
+
+  it should "[Aged Brie] does not change quality when sellIn is negative and quality is at least 50" in {
+    val itemAgedBrie = new Item("Aged Brie", 0, 50)
+    val items = Array[Item](itemAgedBrie)
+    val app = new GildedRose(items)
+
+    app.updateQuality()
+
+    itemAgedBrie.sellIn shouldBe -1
+    itemAgedBrie.quality shouldBe 50
   }
 }
