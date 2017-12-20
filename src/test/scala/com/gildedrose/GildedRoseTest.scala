cc423590633cca38b412e0792955c1dd44e4aac3
v.oliveres <v.oliveres@criteo.com>
2017-12-20 13:00:12 +0100

    Add tests for BackstagePass

--- src/test/scala/com/gildedrose/GildedRoseTest.scala
+++ src/test/scala/com/gildedrose/GildedRoseTest.scala
@@ -81,4 +81,82 @@ class GildedRoseTest extends FlatSpec with Matchers {
     item.quality shouldBe 50
   }
 
+  it should "[BackstagePass] does not change quality when sellIn is higher than 10" in {
+    val item = new Item("Backstage passes to a TAFKAL80ETC concert", 123, 50)
+    val items = Array[Item](item)
+    val app = new GildedRose(items)
+
+    app.updateQuality()
+
+    item.sellIn shouldBe 122
+    item.quality shouldBe 50
+  }
+
+
+  it should "[BackstagePass] increase quality by 1 when sellIn is higher than 10" in {
+    val item = new Item("Backstage passes to a TAFKAL80ETC concert", 11, 35)
+    val items = Array[Item](item)
+    val app = new GildedRose(items)
+
+    app.updateQuality()
+
+    item.sellIn shouldBe 10
+    item.quality shouldBe 36
+  }
+
+  it should "[BackstagePass] increase quality by 2 when sellIn is between 5 and 10" in {
+    val item = new Item("Backstage passes to a TAFKAL80ETC concert", 10, 35)
+    val items = Array[Item](item)
+    val app = new GildedRose(items)
+
+    app.updateQuality()
+
+    item.sellIn shouldBe 9
+    item.quality shouldBe 37
+  }
+
+  it should "[BackstagePass] increase quality by 3 when sellIn is between 1 and 5" in {
+    val item = new Item("Backstage passes to a TAFKAL80ETC concert", 5, 35)
+    val items = Array[Item](item)
+    val app = new GildedRose(items)
+
+    app.updateQuality()
+
+    item.sellIn shouldBe 4
+    item.quality shouldBe 38
+  }
+
+  it should "[BackstagePass] does not change quality when quality is >= 50 and sellIn is higher than 10" in {
+    val item = new Item("Backstage passes to a TAFKAL80ETC concert", 11, 50)
+    val items = Array[Item](item)
+    val app = new GildedRose(items)
+
+    app.updateQuality()
+
+    item.sellIn shouldBe 10
+    item.quality shouldBe 50
+  }
+
+  it should "[BackstagePassdoes not change quality when quality is >= 50 and sellIn is between 5 and 10" in {
+    val item = new Item("Backstage passes to a TAFKAL80ETC concert", 10, 50)
+    val items = Array[Item](item)
+    val app = new GildedRose(items)
+
+    app.updateQuality()
+
+    item.sellIn shouldBe 9
+    item.quality shouldBe 50
+  }
+
+  it should "[BackstagePass] does not change quality when quality is >= 50 and sellIn is between 1 and 5" in {
+    val item = new Item("Backstage passes to a TAFKAL80ETC concert", 5, 50)
+    val items = Array[Item](item)
+    val app = new GildedRose(items)
+
+    app.updateQuality()
+
+    item.sellIn shouldBe 4
+    item.quality shouldBe 50
+  }
+
 }
