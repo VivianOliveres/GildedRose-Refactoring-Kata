80cb503fc1c5591e3b422d1d6a223c17811597b4
v.oliveres <v.oliveres@criteo.com>
2017-12-20 11:49:44 +0100

    Add tests for Sulfuras

--- src/test/scala/com/gildedrose/GildedRoseTest.scala
+++ src/test/scala/com/gildedrose/GildedRoseTest.scala
@@ -5,69 +5,80 @@ import org.scalatest._
 class GildedRoseTest extends FlatSpec with Matchers {
 
   it should "AnyItem decrements quality and decrements sellIn" in {
-    val itemAgedBrie = new Item("SomeItem", 123, 15)
-    val items = Array[Item](itemAgedBrie)
+    val item = new Item("SomeItem", 123, 15)
+    val items = Array[Item](item)
     val app = new GildedRose(items)
 
     app.updateQuality()
 
-    itemAgedBrie.sellIn shouldBe 122
-    itemAgedBrie.quality shouldBe 14
+    item.sellIn shouldBe 122
+    item.quality shouldBe 14
   }
 
   it should "AnyItem decrements quality by 2 and decrements sellIn when sellIn becomes negative" in {
-    val itemAgedBrie = new Item("SomeItem", 0, 15)
-    val items = Array[Item](itemAgedBrie)
+    val item = new Item("SomeItem", 0, 15)
+    val items = Array[Item](item)
     val app = new GildedRose(items)
 
     app.updateQuality()
 
-    itemAgedBrie.sellIn shouldBe -1
-    itemAgedBrie.quality shouldBe 13
+    item.sellIn shouldBe -1
+    item.quality shouldBe 13
   }
 
   it should "AnyItem should never have a negative quality" in {
-    val itemAgedBrie = new Item("SomeItem", 0, 0)
-    val items = Array[Item](itemAgedBrie)
+    val item = new Item("SomeItem", 0, 0)
+    val items = Array[Item](item)
     val app = new GildedRose(items)
 
     app.updateQuality()
 
-    itemAgedBrie.sellIn shouldBe -1
-    itemAgedBrie.quality shouldBe 0
+    item.sellIn shouldBe -1
+    item.quality shouldBe 0
   }
 
   it should "[Aged Brie] increments quality and decrements sellIn" in {
-    val itemAgedBrie = new Item("Aged Brie", 123, 15)
-    val items = Array[Item](itemAgedBrie)
+    val item = new Item("Aged Brie", 123, 15)
+    val items = Array[Item](item)
     val app = new GildedRose(items)
 
     app.updateQuality()
 
-    itemAgedBrie.sellIn shouldBe 122
-    itemAgedBrie.quality shouldBe 16
+    item.sellIn shouldBe 122
+    item.quality shouldBe 16
   }
 
   it should "[Aged Brie] increments quality by 2 and decrements sellIn when sellIn becomes negative" in {
-    val itemAgedBrie = new Item("Aged Brie", 0, 15)
-    val items = Array[Item](itemAgedBrie)
+    val item = new Item("Aged Brie", 0, 15)
+    val items = Array[Item](item)
     val app = new GildedRose(items)
 
     app.updateQuality()
 
-    itemAgedBrie.sellIn shouldBe -1
-    itemAgedBrie.quality shouldBe 17
+    item.sellIn shouldBe -1
+    item.quality shouldBe 17
   }
 
   it should "[Aged Brie] does not change quality when sellIn is negative and quality is at least 50" in {
-    val itemAgedBrie = new Item("Aged Brie", 0, 50)
-    val items = Array[Item](itemAgedBrie)
+    val item = new Item("Aged Brie", 0, 50)
+    val items = Array[Item](item)
     val app = new GildedRose(items)
 
     app.updateQuality()
 
-    itemAgedBrie.sellIn shouldBe -1
-    itemAgedBrie.quality shouldBe 50
+    item.sellIn shouldBe -1
+    item.quality shouldBe 50
+  }
+
+  it should "[Sulfuras] does not change quality neither sellIn" in {
+    val item = new Item("Sulfuras, Hand of Ragnaros", 123, 50)
+    val items = Array[Item](item)
+    val app = new GildedRose(items)
+
+    app.updateQuality()
+
+    item.sellIn shouldBe 123
+    item.quality shouldBe 50
   }
 
 }
