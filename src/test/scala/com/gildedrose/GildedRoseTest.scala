ecd323d7ad96effc3f0914e6caf6583845ca6bb7
v.oliveres <v.oliveres@criteo.com>
2017-12-20 11:47:30 +0100

    Add tests for default items

--- src/test/scala/com/gildedrose/GildedRoseTest.scala
+++ src/test/scala/com/gildedrose/GildedRoseTest.scala
@@ -4,6 +4,39 @@ import org.scalatest._
 
 class GildedRoseTest extends FlatSpec with Matchers {
 
+  it should "AnyItem decrements quality and decrements sellIn" in {
+    val itemAgedBrie = new Item("SomeItem", 123, 15)
+    val items = Array[Item](itemAgedBrie)
+    val app = new GildedRose(items)
+
+    app.updateQuality()
+
+    itemAgedBrie.sellIn shouldBe 122
+    itemAgedBrie.quality shouldBe 14
+  }
+
+  it should "AnyItem decrements quality by 2 and decrements sellIn when sellIn becomes negative" in {
+    val itemAgedBrie = new Item("SomeItem", 0, 15)
+    val items = Array[Item](itemAgedBrie)
+    val app = new GildedRose(items)
+
+    app.updateQuality()
+
+    itemAgedBrie.sellIn shouldBe -1
+    itemAgedBrie.quality shouldBe 13
+  }
+
+  it should "AnyItem should never have a negative quality" in {
+    val itemAgedBrie = new Item("SomeItem", 0, 0)
+    val items = Array[Item](itemAgedBrie)
+    val app = new GildedRose(items)
+
+    app.updateQuality()
+
+    itemAgedBrie.sellIn shouldBe -1
+    itemAgedBrie.quality shouldBe 0
+  }
+
   it should "[Aged Brie] increments quality and decrements sellIn" in {
     val itemAgedBrie = new Item("Aged Brie", 123, 15)
     val items = Array[Item](itemAgedBrie)
@@ -36,4 +69,5 @@ class GildedRoseTest extends FlatSpec with Matchers {
     itemAgedBrie.sellIn shouldBe -1
     itemAgedBrie.quality shouldBe 50
   }
+
 }
